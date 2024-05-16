350e2263e7ca1f71ef3c871eac7ffa46edcccd96
advanced-go <mark.wilson.go@outlook.com>
2024-05-16 13:51:44 -0500

    Initial commit

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/advanced-go/guidance
 
 go 1.21.4
 
-require github.com/advanced-go/stdlib v0.0.0-20240516132802-76b4fd6f5dc5 // indirect
+require github.com/advanced-go/stdlib v0.0.0-20240516132802-76b4fd6f5dc5
+
+require github.com/google/uuid v1.6.0 // indirect
--- http/exchange_test.go
+++ http/exchange_test.go
@@ -1,1 +1,13 @@
 package http
+
+import "fmt"
+
+func ExampleExchange_Invalid() {
+	resp, status := Exchange(nil)
+
+	fmt.Printf("test: Exchange(nil) -> [status:%v] [status-code:%v]\n", status, resp.StatusCode)
+
+	//Output:
+	//test: Exchange(nil) -> [status:Invalid Argument [error: request is nil]] [status-code:500]
+
+}
