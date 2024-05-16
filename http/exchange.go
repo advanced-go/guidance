a3b3d746d4daf8c9bd92aa43f03efaeba1084802
advanced-go <mark.wilson.go@outlook.com>
2024-05-16 09:07:34 -0500

    Adding default resources

--- http/exchange.go
+++ http/exchange.go
@@ -1,6 +1,8 @@
 package http
 
 import (
+	"errors"
+	"fmt"
 	"github.com/advanced-go/guidance/module"
 	"github.com/advanced-go/stdlib/core"
 	"github.com/advanced-go/stdlib/httpx"
@@ -10,18 +12,22 @@ import (
 
 // https://localhost:8081/github/advanced-go/guidance:v1/search?q=golang
 
+var (
+	versionResponse = httpx.NewResponse(core.StatusOK(), core.VersionContent(module.Version))
+)
+
 func Exchange(r *http.Request) (*http.Response, *core.Status) {
-	_, status := httpx.ValidateRequest(r, module.Authority)
-	if !status.OK() {
+	_, path, status0 := httpx.ValidateRequestURL(r, module.Authority)
+	if !status0.OK() {
+		return httpx.NewErrorResponse(status0), status0
+	}
+	switch strings.ToLower(path) {
+	case core.VersionPath:
+		return versionResponse, core.StatusOK()
+	case core.HealthReadinessPath, core.HealthLivenessPath:
+		return httpx.HealthResponseOK, core.StatusOK()
+	default:
+		status := core.NewStatusError(http.StatusNotFound, errors.New(fmt.Sprintf("error invalid URI, resource not found: [%v]", path)))
 		return httpx.NewErrorResponse(status), status
 	}
-	switch strings.ToUpper(r.Method) {
-	case http.MethodGet:
-
-	default:
-		//_, status = activity.PostEntry[*http.Request](r.Header, r.Method, r.URL.Query(), r)
-		//httpx.WriteResponse[E](w, nil, status.HttpCode(), nil)
-		//return status
-	}
-	return nil, nil
 }
