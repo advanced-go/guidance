9ed700abaec027e08cd253b08eccf72920a7e831
advanced-go <mark.wilson.go@outlook.com>
2024-05-23 17:41:52 -0500

    Update package_test.go

--- resiliencyv1/package_test.go
+++ resiliencyv1/package_test.go
@@ -17,85 +17,124 @@ const (
 	authority = "github/advanced-go/documents"
 )
 
-var testEntry = []Entry{
-	{Origin: core.Origin{Region: "region1", Zone: "Zone1", Host: "www.host1.com"}, Status: "active", Timeout: "100ms", RateLimit: "125", RateBurst: "25"},
-	{Origin: core.Origin{Region: "region1", Zone: "Zone2", Host: "www.host2.com"}, Status: "inactive", Timeout: "250ms", RateLimit: "100", RateBurst: "10"},
-	{Origin: core.Origin{Region: "region2", Zone: "Zone1", Host: "www.google.com"}, Status: "removed", Timeout: "3s", RateLimit: "50", RateBurst: "5"},
-}
-
-type documents struct {
-	list []Entry
-}
-
 var (
-	authorityResponse = httpx.NewAuthorityResponse("github/advanced-go/documents")
-	store             = new(documents)
+	testEntry = []Entry{
+		{Origin: core.Origin{Region: "region1", Zone: "Zone1", Host: "www.host1.com"}, Status: "active", Timeout: "100ms", RateLimit: "125", RateBurst: "25"},
+		{Origin: core.Origin{Region: "region1", Zone: "Zone2", Host: "www.host2.com"}, Status: "inactive", Timeout: "250ms", RateLimit: "100", RateBurst: "10"},
+		{Origin: core.Origin{Region: "region2", Zone: "Zone1", Host: "www.google.com"}, Status: "removed", Timeout: "3s", RateLimit: "50", RateBurst: "5"},
+	}
+
+	rsc = NewResource[Entry]("github/advanced-go/documents",
+		func(item any, values url.Values) bool {
+			filter := core.NewOrigin(values)
+			if entry, ok := item.(*Entry); ok {
+				if core.OriginMatch(entry.Origin, filter) {
+					return true
+				}
+			}
+			return false
+		},
+		func(item any, r *http.Request) {
+
+		})
 )
 
-func (d *documents) get(values url.Values) (docs []Entry, status *core.Status) {
-	filter := core.NewOrigin(values)
-	for _, target := range d.list {
-		if core.OriginMatch(target.Origin, filter) {
-			docs = append(docs, target)
+type Resource[T any] struct {
+	List      []T
+	Authority *http.Response
+	Match     func(item any, values url.Values) bool
+	Patch     func(item any, r *http.Request)
+}
+
+func NewResource[T any](authority string, match func(item any, values url.Values) bool, patch func(item any, r *http.Request)) *Resource[T] {
+	r := new(Resource[T])
+	r.Authority = httpx.NewAuthorityResponse(authority)
+	r.Match = match
+	r.Patch = patch
+	return r
+}
+
+func (r *Resource[T]) Length() int {
+	return len(r.List)
+}
+
+func (r *Resource[T]) Get(values url.Values) (items []T, status *core.Status) {
+	if r.Match == nil {
+		return nil, core.NewStatus(core.StatusInvalidArgument)
+	}
+	for _, target := range r.List {
+		if r.Match(&target, values) {
+			items = append(items, target)
 		}
 	}
-	if len(docs) == 0 {
+	if len(items) == 0 {
 		return nil, core.StatusNotFound()
 	}
-	return docs, core.StatusOK()
+	return items, core.StatusOK()
 }
 
-func (d *documents) len() int {
-	return len(d.list)
-}
-
-func (d *documents) add(docs []Entry) *core.Status {
-	if len(docs) > 0 {
-		d.list = append(d.list, docs...)
+func (r *Resource[T]) Put(items []T) *core.Status {
+	if len(items) > 0 {
+		r.List = append(r.List, items...)
 	}
 	return core.StatusOK()
 }
 
-func init() {
-	ctrl := controller.NewController("resiliencyv1", controller.NewPrimaryResource("", authority, time.Second*2, "", do), nil)
-	controller.RegisterController(ctrl)
-}
-
-func do(r *http.Request) (*http.Response, *core.Status) {
+func (r *Resource[T]) Do(req *http.Request) (*http.Response, *core.Status) {
 	//_, path, status1 := httpx.ValidateRequestURL(r, module.Authority)
 	//if !status1.OK() {
 	//	return httpx.NewResponseWithStatus(status1, status1.Err)
 	//}
-	if strings.Contains(r.URL.Path, core.AuthorityPath) {
-		return authorityResponse, core.StatusOK()
+	if strings.Contains(req.URL.Path, core.AuthorityPath) {
+		return r.Authority, core.StatusOK()
 	}
-	switch r.Method {
+	switch req.Method {
 	case http.MethodGet:
-		// Need to add authority
-		docs, status := store.get(r.URL.Query())
+		items, status := r.Get(req.URL.Query())
 		if !status.OK() {
 			return httpx.NewResponseWithStatus(status, nil)
 		}
-		resp, status1 := httpx.NewJsonResponse(docs, r.Header)
+		resp, status1 := httpx.NewJsonResponse(items, req.Header)
 		if !status1.OK() {
 			return httpx.NewResponseWithStatus(status, status.Err)
 		}
 		return resp, core.StatusOK()
 	case http.MethodPut:
-		docs, status := json.New[[]Entry](r.Body, r.Header)
+		items, status := json.New[[]T](req.Body, req.Header)
 		if !status.OK() {
 			return httpx.NewResponseWithStatus(status, nil)
 		}
-		store.add(docs)
+		if len(items) == 0 {
+			return httpx.NewResponseWithStatus(core.StatusNotFound(), nil)
+		}
+		r.Put(items)
+		return httpx.NewResponseWithStatus(core.StatusOK(), nil)
+	case http.MethodPatch:
+		if r.Patch == nil {
+			return httpx.NewResponseWithStatus(core.NewStatus(core.StatusInvalidArgument), nil)
+		}
+		if r.Match == nil {
+			return httpx.NewResponseWithStatus(core.NewStatus(core.StatusInvalidArgument), nil)
+		}
+		for _, target := range r.List {
+			if r.Match(&target, req.URL.Query()) {
+				r.Patch(&target, req)
+			}
+		}
 		return httpx.NewResponseWithStatus(core.StatusOK(), nil)
 	default:
 		return httpx.NewResponseWithStatus(core.StatusNotFound(), nil)
 	}
 }
 
+func init() {
+	ctrl := controller.NewController("resiliencyv1", controller.NewPrimaryResource("", authority, time.Second*2, "", rsc.Do), nil)
+	controller.RegisterController(ctrl)
+}
+
 func ExampleExchange_Simple() {
 	status := put[core.Output](context.Background(), nil, testEntry)
-	fmt.Printf("test: put() -> [status:%v] [count:%v]\n", status, store.len())
+	fmt.Printf("test: put() -> [status:%v] [count:%v]\n", status, rsc.Length())
 
 	values := make(url.Values)
 	values.Add(core.ZoneKey, "zone1")
