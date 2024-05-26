15d236fa882c756347ac913f01629cc01c18420d
advanced-go <mark.wilson.go@outlook.com>
2024-05-26 12:39:45 -0500

    Testing

--- http/resiliency1_test.go
+++ http/resiliency1_test.go
@@ -19,7 +19,10 @@ import (
 
 const (
 	documentsV1Name = module.Ver1 + "/" + module.DocumentsResource
-	documentsV2Name = module.Ver2 + "/" + module.DocumentsResource
+	actionTest      = "test"
+	actionEmpty     = "empty"
+	actionInit      = "init"
+	actionAdd       = "add"
 )
 
 func matchOrigin(item *core.Origin, req *http.Request) bool {
@@ -31,14 +34,14 @@ func matchOrigin(item *core.Origin, req *http.Request) bool {
 }
 
 func mapResiliency(r *http.Request) string {
-	if strings.Contains(r.URL.Path, documentsV2Name) {
-		return documentsV2Name
+	if strings.Contains(r.URL.Path, documentsV1Name) {
+		return documentsV1Name
 	}
 	return documentsV1Name
 }
 
 var (
-	rsc       = httpx.NewResource[core.Origin, httpx.Patch, struct{}](documentsV1Name, matchOrigin, nil, nil, nil)
+	rsc       = httpx.NewResource[core.Origin, struct{}, struct{}](documentsV1Name, matchOrigin, nil, nil, nil)
 	host, err = httpx.NewHost(module.DocumentsAuthority, mapResiliency, rsc.Do)
 )
 
@@ -46,28 +49,44 @@ func init() {
 	if err != nil {
 		fmt.Printf("error: new resource %v", err)
 	}
-	ctrl := controller.NewController("entry-resource", controller.NewPrimaryResource("", module.DocumentsAuthority, time.Second*2, "", rsc.Do), nil)
+	ctrl := controller.NewController("entry-resource", controller.NewPrimaryResource("", module.DocumentsAuthority, time.Second*2, "", host.Do), nil)
 	controller.RegisterController(ctrl)
 }
 
-func Test_resiliencyExchange(t *testing.T) {
+func Test_resiliency1Exchange(t *testing.T) {
 	basePath := "file://[cwd]/httptest/resiliency1/"
 
 	type args struct {
 		req  string
 		resp string
 	}
-	tests := []struct {
-		name string
-		args args
+	workflow := []struct {
+		action string
+		name   string
+		args   args
 	}{
-		{"get-empty", args{req: "get-empty-req.txt", resp: "get-empty-resp.txt"}},
-		{"get-entry-empty", args{req: "get-entry-empty-req.txt", resp: "get-empty-resp.txt"}},
-		{"get-v1", args{req: "get-req-v1.txt", resp: "get-resp-v1.txt"}},
-		{"get-v2", args{req: "get-req-v2.txt", resp: "get-resp-v2.txt"}},
-		{"get-query-v2", args{req: "get-query-req-v2.txt", resp: "get-query-resp-v2.txt"}},
+		{actionInit, "rsc-one", args{req: "document-init.json", resp: ""}},
+		{actionAdd, "rsc-one", args{req: "document-add.json", resp: ""}},
+		{actionEmpty, "rsc-one", args{req: "", resp: ""}},
+
+		/*
+			{actionTest,"get-empty", args{req: "get-empty-req.txt", resp: "get-empty-resp.txt"}},
+			{actionTest,"get-entry-empty", args{req: "get-entry-empty-req.txt", resp: "get-empty-resp.txt"}},
+			{actionTest,"get-v1", args{req: "get-req-v1.txt", resp: "get-resp-v1.txt"}},
+			{actionTest,"get-v2", args{req: "get-req-v2.txt", resp: "get-resp-v2.txt"}},
+			{actionTest,"get-query-v2", args{req: "get-query-req-v2.txt", resp: "get-query-resp-v2.txt"}},
+
+		*/
 	}
-	for _, tt := range tests {
+	for _, tt := range workflow {
+		if tt.action != actionTest {
+			fmt.Printf("action: %v\n", tt.action)
+			switch tt.action {
+			case actionEmpty:
+
+			}
+			continue
+		}
 		failures, req, resp := httpxtest.ReadHttp(basePath, tt.args.req, tt.args.resp)
 		if failures != nil {
 			t.Errorf("ReadHttp() failures = %v", failures)
