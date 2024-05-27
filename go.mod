8d6a6d04d0b1c02c3a0185088610198b3118000e
advanced-go <mark.wilson.go@outlook.com>
2024-05-27 13:22:37 -0500

    Refactoring

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module github.com/advanced-go/guidance
 
 go 1.21.4
 
-require github.com/advanced-go/stdlib v0.0.0-20240527174457-79f3c987d754
+require github.com/advanced-go/stdlib v0.0.0-20240527180214-1711ad37ff46
 
 require github.com/google/uuid v1.6.0 // indirect
--- resiliency1/package.go
+++ resiliency1/package.go
@@ -1,12 +1,16 @@
 package resiliency
 
 import (
+	"fmt"
+	"github.com/advanced-go/guidance/module"
 	"github.com/advanced-go/stdlib/core"
+	"github.com/advanced-go/stdlib/httpx"
 	"net/http"
 )
 
 const (
-	PkgPath = "github/advanced-go/guidance/resiliency1"
+	PkgPath        = "github/advanced-go/guidance/resiliency1"
+	resiliencyName = "resiliency1"
 )
 
 type Entry struct {
@@ -25,4 +29,31 @@ type Entry struct {
 
 type PutBodyConstraints interface {
 	[]Entry | []byte | *http.Request
+}
+
+var (
+	entryContent = httpx.NewListContent[Entry, httpx.Patch, struct{}](matchEntry, nil, nil)
+	entryRsc     = httpx.NewResource2[Entry, httpx.Patch, struct{}](resiliencyName, entryContent, nil)
+	host, err    = httpx.NewHost(module.DocumentsAuthority, mapResource, entryRsc.Do)
+)
+
+func init() {
+	if err != nil {
+		fmt.Printf("error: new resource %v", err)
+	}
+	//ctrl := controller.NewController("entry-resource", controller.NewPrimaryResource("", module.DocumentsAuthority, time.Second*2, "", host.Do), nil)
+	//controller.RegisterController(ctrl)
+}
+
+func matchEntry(req *http.Request, item *Entry) bool {
+	filter := core.NewOrigin(req.URL.Query())
+	if core.OriginMatch(item.Origin, filter) {
+		return true
+	}
+	return false
+}
+
+func mapResource(r *http.Request) string {
+	return resiliencyName
+
 }
