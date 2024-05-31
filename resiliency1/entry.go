79514b33f0f82b4141396f47b2f263aa3ce84ce6
advanced-go <mark.wilson.go@outlook.com>
2024-05-31 10:02:10 -0500

    Updating

---
+++ resiliency1/entry.go
@@ -0,0 +1,20 @@
+package resiliency1
+
+import "github.com/advanced-go/stdlib/core"
+
+type Entry struct {
+	Origin    core.Origin
+	Status    string `json:"status"`
+	CreatedTS string `json:"created-ts"`
+	UpdatedTS string `json:"updated-ts"`
+
+	// Timeout
+	Timeout string `json:"timeout"`
+
+	// Rate Limiting
+	RateLimit string `json:"rate-limit"`
+	RateBurst string `json:"rate-burst"`
+}
+
+type PostData struct {
+}
--- resiliency1/package.go
+++ resiliency1/package.go
@@ -33,23 +33,6 @@ var (
 	}
 )
 
-type Entry struct {
-	Origin    core.Origin
-	Status    string `json:"status"`
-	CreatedTS string `json:"created-ts"`
-	UpdatedTS string `json:"updated-ts"`
-
-	// Timeout
-	Timeout string `json:"timeout"`
-
-	// Rate Limiting
-	RateLimit string `json:"rate-limit"`
-	RateBurst string `json:"rate-burst"`
-}
-
-type PostData struct {
-}
-
 // Get - resource GET
 func Get(ctx context.Context, h http.Header, url *url.URL) ([]Entry, *core.Status) {
 	if url == nil || !strings.HasPrefix(url.Path, module.ResiliencyResource) {
