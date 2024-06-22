e1cd32fe1d0cd2488cc7047f60fbcebbc160df69
advanced-go <mark.wilson.go@outlook.com>
2024-06-22 10:27:18 -0500

    Adding scheduling

---
+++ scheduling/todo.go
@@ -0,0 +1,7 @@
+package scheduling
+
+// Need scheduling for resiliency to determine when to not run, as in the case of a data center
+// stress testing.
+
+// Need scheduling for region work, to determine how often and when to look for new services.
+// Maybe tied to a client release cycle.
