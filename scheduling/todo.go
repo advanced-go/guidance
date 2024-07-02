98bfdcf1427c942988056934d790e22c6d686c9b
advanced-go <mark.wilson.go@outlook.com>
2024-07-02 07:49:39 -0500

    Adding scheduling

--- scheduling/todo.go
+++
@@ -1,7 +0,0 @@
-package scheduling
-
-// Need scheduling for resiliency to determine when to not run, as in the case of a data center
-// stress testing.
-
-// Need scheduling for region work, to determine how often and when to look for new services.
-// Maybe tied to a client release cycle.
---
+++ scheduling1/package.go
@@ -0,0 +1,7 @@
+package scheduling1
+
+const (
+	PkgPath   = "github/advanced-go/guidance/scheduling1"
+	routeName = "documents-resiliency"
+	hostKey   = "docs-host"
+)
