c1ef759b43a66db2acef42d37e1c07875b2a9217
advanced-go <mark.wilson.go@outlook.com>
2024-07-21 09:53:06 -0500

    Updating

--- controller1/rowset.go
+++ controller1/rowset.go
@@ -1,19 +1,33 @@
 package controller1
 
+type RoutingPolicy struct {
+	Primary     bool   `json:"primary"`
+	Authority   string `json:"authority"` // github/advanced-go/observation: provider/account/repository
+	AuthVersion string `json:"auth-version"`
+	Prioritize  string `json:"prioritize"` // Origin, availability
+	// OriginThreshold - when to prioritize by availability instead of origin
+	OriginThreshold int `json:"origin-threshold"`
+}
+
 type Rowset struct {
 	Version              string `json:"version"`
 	ProcessingScheduleId string `json:"processing-schedule-id"`
 	DependencyUpdates    bool   `json:"dependency-updates"`
 	DependencyScheduleId string `json:"dependency-schedule-id"`
 
-	RouteName    string `json:"route"`
-	RateLimiting bool   `json:"rate-limiting"`
-	RegionT      string `json:"region-t"`
-	ZoneT        string `json:"zone-t"`
-	SubZoneT     string `json:"sub-zone-t"`
-	HostT        string `json:"host-t"`
-	Authority    string `json:"authority"` // github/advanced-go/observation: provider/account/repository
-	AuthVersion  string `json:"auth-version"`
+	RouteName string `json:"route"`
+	//RateLimiting bool   `json:"rate-limiting"`
+	//RegionT      string `json:"region-t"`
+	//ZoneT        string `json:"zone-t"`
+	//SubZoneT     string `json:"sub-zone-t"`
+	//HostT        string `json:"host-t"`
+	// Need to determine a cost metric. This choice is between consistency vs availability.
+	// All consistency means only local zone routing.
+	// Startup
+	//Authority      string        `json:"authority"` // github/advanced-go/observation: provider/account/repository
+	//AuthVersion    string        `json:"auth-version"`
+	StartupPolicy  RoutingPolicy `json:"startup-policy"`
+	FailoverPolicy RoutingPolicy `json:"failover-policy"`
 
 	// Notifications
 	Email string
