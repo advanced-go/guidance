30a34b20ed460e6f291823d423fd30e11ac060ba
advanced-go <mark.wilson.go@outlook.com>
2024-08-22 12:54:21 -0500

    Adding SQL command to plans

--- resiliency1/plan.go
+++ resiliency1/plan.go
@@ -9,22 +9,24 @@ import "time"
 // Value == 0  -> no threshold, re-routing immediately
 // Value > 0   -> re-routing when threshold is met
 type FailoverPlan struct {
-	EntryId   int       `json:"entry-id"`
-	RouteName string    `json:"route"`
-	AgentId   string    `json:"agent-id"`
-	CreatedTS time.Time `json:"created-ts"`
-	Scope     string    `json:"scope"` // SubZone, Zone, Region, *, empty or none -> not configured
-	Threshold int       `json:"threshold"`
+	EntryId    int       `json:"entry-id"`
+	RouteName  string    `json:"route"`
+	AgentId    string    `json:"agent-id"`
+	SQLCommand string    `json:"sql-command"` // insert,update,delete
+	CreatedTS  time.Time `json:"created-ts"`
+	Scope      string    `json:"scope"` // SubZone, Zone, Region, *, empty or none -> not configured
+	Threshold  int       `json:"threshold"`
 }
 
 // RedirectPlan - ingress redirect
 // TODO : need to add CDC to this table for Agent data change notifications.
 type RedirectPlan struct {
-	EntryId   int       `json:"entry-id"`
-	RouteName string    `json:"route"`
-	AgentId   string    `json:"agent-id"`
-	CreatedTS time.Time `json:"created-ts"`
-	UpdatedTS time.Time `json:"updated-ts"`
-	Location  string    `json:"location"`
-	Status    string    `json:"status"` // Scheduled,In-Progress,Completed,Failed
+	EntryId    int       `json:"entry-id"`
+	RouteName  string    `json:"route"`
+	AgentId    string    `json:"agent-id"`
+	SQLCommand string    `json:"sql-command"` // insert,update,delete
+	CreatedTS  time.Time `json:"created-ts"`
+	UpdatedTS  time.Time `json:"updated-ts"`
+	Location   string    `json:"location"`
+	Status     string    `json:"status"` // Scheduled,In-Progress,Completed,Failed
 }
